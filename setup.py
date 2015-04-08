d06aaca66c4d9cb3f471a59689ed9f15f0f470a3
S.Turgut <safaktrgt@gmail.com>
2015-04-08 13:22:08 +0300

    initial commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='email_templates',
+    version='0.1.0',
+    author='',
+    author_email='',
+    packages=find_packages(),
+    url='',
+    description='Django email templates',
+    long_description=open('README.md').read(),
+    install_requires=[],
+    dependency_links=[],
+    include_package_data=True,
+    zip_safe=False,
+    platforms=['any'],
+)
