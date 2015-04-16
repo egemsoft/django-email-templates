f266fbaf8f13b780f3ba23974330f4a19bddd6dc
S.Turgut <safaktrgt@gmail.com>
2015-04-16 15:21:32 +0300

    version bump 0.2.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='email_templates',
-    version='0.1.0',
+    version='0.2.1',
     author='',
     author_email='',
     packages=find_packages(),
